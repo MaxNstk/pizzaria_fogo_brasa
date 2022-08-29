34c2db5fcf79bbd85d284bf1f422f565d77e535f
Max <max.starke@singular.inf.br>
2022-08-29 20:25:08 -0300

    config projeto

---
+++ pizzaria_fogo_brasa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pizzaria_fogo_brasa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzaria_fogo_brasa.settings')
+
+application = get_asgi_application()

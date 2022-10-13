d436fca9575703b995fa4c0944f13be99ab4ab9e
Max <maxnatanaelstk@gmail.com>
2022-10-12 22:21:06 -0300

    implemnentação de consistencia

--- core/management/commands/create_initial_data.py
+++ core/management/commands/create_initial_data.py
@@ -5,13 +5,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         
-        admin = User.objects.create_user(username='admin',password='admin123',is_superuser=True,
-        first_name='admin',email='admin@pizzariafogobrasa.com', phone_number='99999-9999', cpf_cnpj='99.999.999/9999-99')
-        max = User.objects.create_user(username='max',password='max123',first_name='João',last_name='Krieger',
-        email='max@pizzariafogobrasa.com', phone_number='99999-9999', cpf_cnpj='000.000.000-99')
-        joao = User.objects.create_user(username='max',password='joao123',first_name='Max',last_name='Starke',
-        email='joao@pizzariafogobrasa.com', phone_number='88888-888', cpf_cnpj='000.000.000-00')
-
+        try:
+            admin = User.objects.create_user(username='admin',password='admin123',is_superuser=True,
+            first_name='admin',email='admin@pizzariafogobrasa.com', phone_number='99999-9999', cpf_cnpj='99.999.999/9999-99')
+            max = User.objects.create_user(username='max',password='max123',first_name='João',last_name='Krieger',
+            email='max@pizzariafogobrasa.com', phone_number='99999-9999', cpf_cnpj='000.000.000-99')
+            joao = User.objects.create_user(username='max',password='joao123',first_name='Max',last_name='Starke',
+            email='joao@pizzariafogobrasa.com', phone_number='88888-888', cpf_cnpj='000.000.000-00')
+        except:
+            admin = User.objects.get(username='admin')
+            max = User.objects.get(username='max')
+            joao = User.objects.get(username='joao')
+            
         size_gg = Size.objects.get_or_create(short_name='GG', description='Gigante')
 
         flavor_calabresa =  Flavor.objects.get_or_create(name='Calabresa', description='Molho de tomate, queijo mussalera e muita calabresa!')

bbfd01eea5e4f13141bd6a4237977fd506d2647e
Max <maxnatanaelstk@gmail.com>
2022-10-28 21:54:01 -0300

    adição dos preços nos modelos de relacionamento

--- core/models/order_pizza.py
+++ core/models/order_pizza.py
@@ -4,8 +4,10 @@ from django.db import models
 class OrderPizza(models.Model):
     pizza = models.ForeignKey('Pizza', verbose_name='Pizza', on_delete=models.DO_NOTHING)
     order = models.ForeignKey('Order', verbose_name='Pedido',  on_delete=models.DO_NOTHING)
+    pizza_price = models.FloatField()
     
     def save(self, *args, **kwargs) -> None:
-        save = super().save(*args, **kwargs)
+        self.pizza_price = self.pizza.price
+        super().save(*args, **kwargs)
         self.order.save()
-        return save
+    
--- core/models/order_product.py
+++ core/models/order_product.py
@@ -1,11 +1,11 @@
-
 from django.db import models
 
 class OrderProduct(models.Model):
     product = models.ForeignKey('Product', verbose_name='Produto', on_delete=models.DO_NOTHING)
     order = models.ForeignKey('Order', verbose_name='Pedido',  on_delete=models.DO_NOTHING)
-    
+    product_price = models.FloatField()
+
     def save(self, *args, **kwargs) -> None:
-        save = super().save(*args, **kwargs)
+        self.product_price = self.product.price
+        super().save(*args, **kwargs)
         self.order.save()
-        return save

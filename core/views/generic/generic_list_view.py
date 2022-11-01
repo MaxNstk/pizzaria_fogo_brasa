dc0e8f412d1374d336a3f02da627509e785b94fa
Max <maxnatanaelstk@gmail.com>
2022-11-01 19:43:09 -0300

    paginação

--- core/views/generic/generic_list_view.py
+++ core/views/generic/generic_list_view.py
@@ -1,11 +1,20 @@
 from django.views.generic import ListView
+from django.core.paginator import Paginator
+
 
 class GenericListView(ListView):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    entries_per_page = 10
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
         context['headers'] = self.set_table_headers()
+
+        paginator = Paginator(self.get_queryset(), self.entries_per_page)
+        page_number = self.request.GET.get('page', 1)
+        context['page_obj'] = paginator.get_page(page_number)
         return context
 
     def set_table_headers(self):
         return None
+    

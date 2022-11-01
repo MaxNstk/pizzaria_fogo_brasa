1b964d5eaab268df76969b00e7fe99843b13c2e3
Max <maxnatanaelstk@gmail.com>
2022-11-01 20:25:26 -0300

    utilizando a paginação padrão do django

--- core/views/size/size_list_view.py
+++ core/views/size/size_list_view.py
@@ -5,7 +5,7 @@ from core.views.generic.generic_list_view import GenericListView
 class SizeListView(GenericListView):
 
     template_name = 'size_list.html'
-
+    
     def get_queryset(self):
         return Size.objects.all()
 

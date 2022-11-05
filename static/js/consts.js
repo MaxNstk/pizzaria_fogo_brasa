b1f84caf0c652b07ab236e91a930ec44e7af1161
Max <maxnatanaelstk@gmail.com>
2022-11-05 11:49:46 -0300

    ativando e desativando objetos através da listagem

---
+++ static/js/consts.js
@@ -0,0 +1,3 @@
+const USER_PATH = 'api/v1/user/'
+const SIZE_PATH = 'api/v1/size/'
+const FLAVOR_PATH = 'api/v1/flavor/'
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,10 +1,10 @@
-async function blockUser(userId, is_active){
+async function handleObjectActivation(objectId, IsActive, path){
     const response =  await axios({
         headers: {"X-CSRFToken": CONTEXT.CSRF_TOKEN,},
-        url : `api/v1/user/${userId}/`,
+        url : `${path}${objectId}/`,
         method : 'patch',    
         datatype: 'json',
-        data: {'is_active': !is_active},
+        data: {'is_active': !IsActive},
         })
     if (response.status == 200){
         window.location.reload()

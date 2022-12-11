893b199b2bd4dfa7dbe51fad17480df03c6be239
JEKrieger <joaoeduardokrieger123@gmail.com>
2022-12-11 11:20:05 -0300

    Desenvolvida a tela de realizar pedido

--- core/forms/order_form.py
+++ core/forms/order_form.py
@@ -24,6 +24,6 @@ class OrderForm(FormHandlerMixin, ModelForm):
                  , css_class='row'
              ),       
              Div(
-                 Div(Submit('', 'CONCLUIR PEDIDO', css_class='btn btn-primary w-100 button-form',css_id="btn-create-order"), css_class='col-lg-6'),
+                 Div(Submit('', 'CONCLUIR PEDIDO', css_class='btn btn-primary w-100 mt-5 button-form',css_id="btn-create-order"), css_class='col-lg-12'),
                  css_class='row justify-content-between mb-5')
          )
